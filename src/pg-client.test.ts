fe73b6bffb9aa1c4b650347cd1caf7811218c188
harrytang <harry@powerkernel.com>
2022-01-31 19:27:34 +0200

    chore: add tests

--- src/pg-client.test.ts
+++ src/pg-client.test.ts
@@ -11,3 +11,23 @@ it('should throw error if not connected to the DB server', () => {
     PgClient.client;
   }).toThrow();
 });
+
+it('connects to the DB server', () => {
+  const spyConnect = jest.spyOn(PgClient, 'connect').mockImplementation();
+  PgClient.connect({
+    host: 'localhost',
+    port: 4532,
+    user: 'postgres',
+    password: 'YOUR_PASSWORD',
+  });
+
+  expect(spyConnect).toHaveBeenCalledTimes(1);
+});
+
+it('closes the connection', () => {
+  const spyClose = jest.spyOn(PgClient, 'close').mockImplementation();
+
+  PgClient.close();
+
+  expect(spyClose).toHaveBeenCalledTimes(1);
+});
